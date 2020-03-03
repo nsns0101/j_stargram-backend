3f5a00fe127c005dd3b9feb5f147ccb1d7f0fcc7
nsns0101 <nsns0101@naver.com>
2020-03-03 19:09:37 +0900

    end_toggleLike()

--- src/api/Like/toggleLike/toggleLike.js
+++ src/api/Like/toggleLike/toggleLike.js
@@ -1,3 +1,4 @@
+//좋아요 투표시 실행되는 함수
 import { isAuthenticated } from "../../../middlewares";
 import { prisma } from "../../../../generated/prisma-client";
 
@@ -11,34 +12,35 @@ export default {
 
       const { postId } = args;
       const { user } = request;
+      //투표하였는지를 판단하기 위한 옵션
+      const filterOption = {
+        AND: [
+          //조건1 : 사용자 ID를 가진 좋아요가 이미 있는 경우
+          {
+            user: {
+              id: user.id
+            }
+          },
+          //조건2 : 게시글 ID를 가진 게시물이 있을 때
+          {
+            post: {
+              id: postId
+            }
+          }
+        ]
+      };
       //prisma의 $exists는 사용중인지를 체크
       try {
         //좋아요 투표를 했는지 하지않았는지를 확인
-        const existingLike = await prisma.$exists.like({
-          //둘다 만족할 때(현재 게시글에 유저가 투표를 했는지)
-          AND: [
-            //조건1 : 사용자 ID를 가진 좋아요가 이미 있는 경우
-            {
-              user: {
-                id: user.id
-              }
-            },
-            //조건2 : 게시글 ID를 가진 게시물이 있을 때
-            {
-              post: {
-                id: postId
-              }
-            }
-          ]
-        });
-        // existingLike = TRUE
+        const existingLike = await prisma.$exists.like(filterOption);
+
+        //좋아요를 이미 투표하였을 경우 투표한 것을 삭제(-1)
         if (existingLike) {
-          //todo
+          await prisma.deleteManyLikes(filterOption);
         }
-        // existingLike = FALSE
+        //좋아요 투표를 하지 않았을 경우 투표를 추가(+1)
         else {
-          //투표안했으면 좋아요를 생성
-          const newLike = await prisma.createLike({
+          await prisma.createLike({
             user: {
               connect: {
                 id: user.id
