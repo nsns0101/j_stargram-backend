e4c2b070657452d061662f86039bdcec91006361
nsns0101 <nsns0101@naver.com>
2020-03-04 20:01:56 +0900

    update_seeMyProfile()

--- src/api/User/seeMyProfile/seeMyProfile.js
+++ src/api/User/seeMyProfile/seeMyProfile.js
@@ -3,11 +3,16 @@ import { USER_FRAGMENT } from "../../../fragments";
 export default {
   Query: {
     //DoubleUnderScore(__)는 부모의 arguments를 뜻함
-    seeMyProfile: (_, __, { request, isAuthenticated, prisma }) => {
+    seeMyProfile: async (_, __, { request, isAuthenticated, prisma }) => {
       isAuthenticated(request);
       const { user } = request;
-      //현재 사용자의 정보를 가져옴
-      return prisma.user({ id: user.id }).$fragment(USER_FRAGMENT);
+      const myProfile = await prisma.user({ id: user.id }); //내 프로필 정보
+      const myPosts = await prisma.user({ id: user.id }).posts(); //내 게시글 정보
+      //이 파일의 graph.ql참고
+      return {
+        user: myProfile,
+        post: myPosts
+      };
     }
   }
 };
