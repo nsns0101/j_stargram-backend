b331fdbe44f4fec3c246059c9ea8b7bee25532d5
nsns0101 <nsns0101@naver.com>
2020-03-06 21:16:38 +0900

    end_seeRooms()

---
+++ src/api/seeRooms/seeRooms.js
@@ -0,0 +1,20 @@
+//채팅방 전부보기
+export default {
+  Query: {
+    seeRooms: async (_, __, { request, isAuthenticated, prisma }) => {
+      isAuthenticated(request);
+      const { user } = request; //로그인 유저
+
+      //나의 채팅방을 반환
+      return prisma.rooms({
+        where: {
+          //participants = 채팅 참가자(models.graphql참고)
+          //_some = 예를 들어 [1,2,3,4]가 있는데 이 4개 중 하나라도 포함된 개체를 찾음
+          participants_some: {
+            id: user.id
+          }
+        }
+      });
+    }
+  }
+};
